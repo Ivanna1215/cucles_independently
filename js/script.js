749e2725967b402b38b58af8b31bbff826455575
Ivanna_1109 <ivanna.kovaliv1215@gmail.com>
2022-01-04 11:15:15 +0300

    commit1

---
+++ js/script.js
@@ -0,0 +1,11 @@
+'use strict'
+function amountNumber() {
+    let i = +prompt('Input your OneNum');
+    let j = +prompt('Input your twoNUm');
+    let result = 0;
+for (i;i<j;i++) {
+    result = result + i;
+}
+alert (`Your result is ${result}`);
+
+}
